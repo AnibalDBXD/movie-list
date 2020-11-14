b4e071f3d75f8c16c423e0737db6566cef04c2dc
AnibalDBXD <olivaresluisanibal@gmail.com>
2020-11-14 16:38:45 -0400

    Fix React Router

--- src/app/routers/App.js
+++ src/app/routers/App.js
@@ -1,9 +1,12 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 
 import Home from "../pages/Home";
 import About from "../pages/About";
 import Layout from "../components/Layout";
+
+const error404 = () => <h1>No Found</h1>;
+
 export default function App() {
   return (
     <Router>
@@ -11,6 +14,7 @@ export default function App() {
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/about" component={About} />
+          <Route component={error404} />
         </Switch>
       </Layout>
     </Router>
